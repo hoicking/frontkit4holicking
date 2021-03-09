f3867b907e0ab7b81f33cfbf7ea3fa867d4e57e4
hoicking <759387240@qq.com>
2021-03-09 11:00:36 +0800

    use hooks

---
+++ src/playground.js
@@ -0,0 +1,9 @@
+const a = '[]{}<>'
+const b = '{<>}'
+
+
+const array = a.split('')
+
+const left = ['{', '[', '<']
+
+const right
--- src/usehooks.js
+++ src/usehooks.js
@@ -1,1 +1,45 @@
-import {} from 'react'
+import {useRef, useEffect} from 'react'
+
+const useCurrentValue = (value) =>{
+    const valueRef = useRef(value)
+
+    useEffect(()=>{
+        valueRef.current = value
+    }, [value])
+
+    return valueRef
+}
+
+const useTitle = (title) => {
+    useEffect(() => {
+      document.title = title
+    }, [])
+
+    return
+  }
+
+  const useUpdate = () =>{
+    const [,setFlag] = useState()
+    const update = () =>{
+        setFlag(new Date())
+    }
+
+    return update
+}
+
+
+const useScroll = (scrollRef) => {
+    const [pos, setPos] = useState([0,0])
+  
+    useEffect(() => {
+      function handleScroll(e){
+        setPos([scrollRef.current.scrollLeft, scrollRef.current.scrollTop])
+      }
+      scrollRef.current.addEventListener('scroll', handleScroll, false)
+      return () => {
+        scrollRef.current.removeEventListener('scroll', handleScroll, false)
+      }
+    }, [])
+  
+    return pos
+  }
